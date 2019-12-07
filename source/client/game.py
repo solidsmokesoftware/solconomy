985cba7e8668fbe3246e6f9c8c268782e2a8d96f
Solid Smoke <solid.smoke.software@gmail.com>
2019-12-06 16:35:09 -0800

    0.002 Communication Protocol
    Added a packet parser and builder to handle messages and hooked it into the client. Rewrote the communication protocol to something simpler, based on Classic Minecraft

--- source/client/game.py
+++ source/client/game.py
@@ -20,6 +20,7 @@ import time
 from threading import Thread
 from source.common.clock import Clock
 from source.common.messenger import Messenger
+from source.common.communication import Parser
 
 
 class Game:
@@ -30,6 +31,7 @@ class Game:
       self.batch = sprites.batch
       self.clock = Clock(1.0/10.0)
       self.network = None
+      self.parser = Parser()
       self.incoming = Messenger()
       self.incoming_handler = Thread(target=self.handle_incoming)
       self.run_handler = Thread(target=self.run)
@@ -61,22 +63,17 @@ class Game:
       self.camera = Camera()
       self.events = EventHandler(self)
 
-      self.options = {LOGIN_RES.encode(): self.handle_login,
-                  POS_UPDATE_RES.encode(): self.handle_pos_update,
-                  ACCEPT_RES.encode(): self.handle_accept,
-                  DECLINE_RES.encode(): self.handle_decline,
-                  MESSAGE_RES.encode(): self.handle_player_message,
-                  BATTLE_RES.encode(): self.handle_battle,
-                  CHALLENGE_RES.encode(): self.handle_challenge,
-                  POS_INFO_RES.encode(): self.handle_pos_info,
-                  PART_INFO_RES.encode(): self.handle_part_info,
-                  FULL_INFO_RES.encode(): self.handle_full_info,
-                  EQUIP_INFO_RES.encode(): self.handle_equip_info,
-                  IDLE_RES.encode(): self.handle_idle,
-
-                  MAKE_ACTOR_COM.encode(): self.handle_make_actor,
-                  DEL_ACTOR_COM.encode(): self.handle_del_actor
-                  }
+      self.options = {            
+         #SERVER_IDENT.encode(): self.handle_ident,
+         SERVER_POS.encode(): self.handle_pos,
+         SERVER_PING.encode(): self.handle_ping,
+         SERVER_LEVEL.encode(): self.handle_level,
+         SERVER_BLOCK.encode(): self.handle_block,
+         SERVER_NEW_ACTOR.encode(): self.handle_new_actor,
+         SERVER_DEL_ACTOR.encode(): self.handle_del_actor,
+         SERVER_KICK.encode(): self.handle_kick, 
+         SERVER_MSG.encode(): self.handle_message
+         }
 
    def start(self):
       pyglet.app.run()
@@ -93,19 +90,21 @@ class Game:
    def start_connection(self):
       print("Game: Starting connection")
       
-      msg = f"{self.username}/{self.password}/{LOGIN_COM}/0"
-      self.network.send_data(msg.encode())
+      data = f"{self.username}/{self.password}"
+      msg = self.parser.encode(PLAYER_IDENT, data, 0)
+      self.network.send_data(msg)
       packet = self.network.recv()
-      data = packet.data.split(b"/")
-      self.handle_login(data)
+      self.handle_ident(packet)
 
       self.incoming_handler.start() #Thread that calls handle_incoming to run the network
       self.run_handler.start()
       self.events.start()
 
-   def handle_login(self, data):
+   def handle_ident(self, packet):
       print('Game: Logging into server')
-      values = data[0].split(b":")
+      data = self.parser.decode(packet.data)
+      values = self.parser.pos(data[0])
+
       seed = int(data[1])
 
       index = int(values[0])
@@ -134,7 +133,7 @@ class Game:
       while True:
          packet = self.network.recv()
          if packet:
-            packet.data = packet.data.split(b"/")
+            packet.data = self.parser.decode(packet.data)
             self.incoming.give(packet)
          time.sleep(SMALL_NUMBER)
 
@@ -147,99 +146,65 @@ class Game:
             packets = self.incoming.get()
             for packet in packets:
                #try:
-               print(f"Game: Packet {packet.data} from {packet.host}")
+               #print(f"Game: Packet {packet.data} from {packet.host}")
                self.server_tick = int(packet.data[-1])
-               self.options[packet.data[-2]](packet.data)
+               self.options[packet.data[-2]](packet)
                #except:
                #   print(f"Game: Bad packet {packet.data} from {packet.host}")
             
             self.update_server(delta)
 
-   def handle_pos_update(self, data):
-      print(f"Game: Handling pos update {data}")
+   def handle_pos(self, packet):
+      #print(f"Game: Handling pos update {packet.data}")
 
-      objects = data[0].split(b"-")
+      object = self.parser.split(packet.data[0])
 
-      for object in objects:
-         print(f"Game: step {object}")
-         values = object.split(b":")
-         index = int(values[0])
-         x = int(values[0])
-         y = int(values[1])
-         pos = x * TILE_SIZE, y * TILE_SIZE
-         self.close_actors.append([index, pos])
+      #print(f"Game: step {object}")
+      values = self.parser.pos(object)
+      index = int(values[0])
+      x = int(values[0])
+      y = int(values[1])
+      pos = x * TILE_SIZE, y * TILE_SIZE
+      self.close_actors.append([index, pos])
 
-         if index in self.world.actors:
-            if index == self.player.index:
-               pass
-            else:
-               actor = self.world.actors[index]
-               self.world.actors.move(actor, x, y)
+      if index in self.world.actors:
+         if index == self.player.index:
+            pass
          else:
-            image = sprites.make_image('soul.png')
-            sprite = sprites.make_actor(image, x, y)
-            actor = Actor(index, x, y, sprite)
-            self.world.actors[id] = actor
+            actor = self.world.actors[index]
+            self.world.actors.move(actor, x, y)
+      else:
+         image = sprites.make_image('soul.png')
+         sprite = sprites.make_actor(image, x, y)
+         actor = Actor(index, x, y, sprite)
+         self.world.actors[id] = actor
 
    def update_server(self, delta):
-      print('Game: Updating server')
       state = self.actor.get_state()
-      msg = f"{state}/{POS_UPDATE_COM}/{self.server_tick}"
-      self.network.send_data(msg.encode())
+      #print(f"Game: Updating server {state}")
+      msg = self.parser.encode(PLAYER_POS, state, self.server_tick)
+      self.network.send_data(msg)
 
-   def handle_bad_message(self, data):
-      return
+   def handle_message(self, packet):
+      print(packet.data)
 
-   def handle_accept(self, data):
-      return
+   def handle_ping(self, packet):
+      print(packet.data)
 
-   def handle_decline(self, data):
-      return
+   def handle_new_actor(self, packet):
+      print(packet.data)
 
-   def handle_player_message(self, data):
-      return
+   def handle_del_actor(self, packet):
+      print(packet.data)
 
-   def handle_battle(self, data):
-      print(data)
+   def handle_block(self, packet):
+      print(packet.data)
 
-   def handle_challenge(self, data):
-      print(data)
+   def handle_level(self, packet):
+      print(packet.data)
 
-   def handle_pos_info(self, data):
-      print(data)
-
-   def handle_part_info(self, data):
-      print(data)
-
-   def handle_equip_info(self, data):
-      print(data)
-
-   def handle_full_info(self, data):
-      print(data)
-
-      index = data[0]
-      full_name = data[1]
-      subrace = data[2]
-      warband = data[3]
-      rank = data[4]
-      level = data[5]
-      x = data[6]
-      y = data[7]
-      hp = data[8]
-      hp_max = data[9]
-      datas = [full_name, level, subrace, rank, warband, hp, hp_max]
-      data = '%s\n%s, %s\n%s, %s\n%s/%s' % datas
-      player.view(data)
-
-   def handle_idle(self, data):
-      print(data)
-
-   def handle_make_actor(self, data):
-      print(data)
-
-   def handle_del_actor(self, data):
-      print(data)
-
+   def handle_kick(self, packet):
+      print(packet.data)
 
 
 
